69ff8702d82079e8c942d0b04013b9642fb27039
Vladimir Molchanov <r1ken@mail.ru>
2024-10-05 11:10:42 +0300

    fix isort

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -6,11 +6,8 @@ from app.core.db import get_async_session
 from app.core.google_client import get_service
 from app.core.user import current_superuser
 from app.crud.charity_project import project_crud
-from app.services.google_api import (
-    set_user_permissions,
-    spreadsheets_create,
-    spreadsheets_update_value,
-)
+from app.services.google_api import (set_user_permissions, spreadsheets_create,
+                                     spreadsheets_update_value)
 
 router = APIRouter()
 
